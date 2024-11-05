f626e883e3b11cc165b73790f1f11c7962bec824
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2024-11-05 16:50:04 -0500

    feat: SearchScreen

---
+++ lib/src/features/recipes/presentation/screens/search_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'themealsdb',
+            style: TextStyle(
+              color: color.onPrimary,
+            ),
+          ),
+          backgroundColor: color.primary,
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          color: color.primary,
+          child: const Column(
+            children: [
+              SearchBar(
+                hintText: 'Ingrese el nombre de la comida',
+                leading: Icon(Icons.search),
+              ),
+            ],
+          ),
+        ));
+  }
+}
